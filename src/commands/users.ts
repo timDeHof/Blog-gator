426e0f239f86f9583bac584e40fc0068d5e9157f
timDeHof <ttdehof@gmail.com>
2025-12-28 23:53:35 -0500

    feat(commands): ✨ Implement command registration and execution
    * Added `registerCommand` and `runCommand` functions for command handling.
    * Introduced `handlerLogin` for user login functionality.
    * Updated `main` function to utilize command registry.

---
+++ src/commands/users.ts
@@ -0,0 +1,10 @@
+import { setUser } from "../config";
+export function handlerLogin(cmdName: string, ...args: string[]): void {
+  if (args.length !== 1) {
+    throw new Error(`usage ${cmdName} <name>`);
+  }
+
+  const userName = args[0];
+  setUser(userName);
+  console.log(`User switched successfully`);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,32 @@
-import { readConfig, setUser } from "./config";
+import {
+  type CommandsRegistry,
+  registerCommand,
+  runCommand,
+} from "./commands/commands";
+import { handlerLogin } from "./commands/users";
 function main() {
-  setUser("TimothyDeHof");
-  const config = readConfig();
-  console.log(config);
+  const args = process.argv.slice(2);
+  if (args.length < 1) {
+    console.log("usage: cli <command> [args...]");
+    process.exit(1);
+  }
+
+  const cmdName = args[0];
+  const cmdArgs = args.slice(1);
+  const commandsRegistry: CommandsRegistry = {};
+
+  registerCommand(commandsRegistry, "login", handlerLogin);
+
+  try {
+    runCommand(commandsRegistry, cmdName, ...cmdArgs);
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(`Error running command ${cmdName}: ${err.message}`);
+    } else {
+      console.error(`Error running command ${cmdName}: ${err}`);
+    }
+    process.exit(1);
+  }
 }
 
 main();
